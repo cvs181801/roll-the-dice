d1f707d15120602923ceca509d659855f17ef399
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-30 10:11:10 -0700

    try creating random # generator a diff way

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 //create a way to generate a random number
 
 function randomNum() {
-    const result = Math.floor(Math.random()* 6);
-    if (result >= 1) {
-        return result;
-    } else {
-        return 6;
-    }
+    // const result = Math.floor(Math.random()* 6);
+    // if (result >= 1) {
+    //     return result;
+    // } else {
+    //     return 6;
+    // }
+
+    let randomNumber = getRandomInt(1,7);
+    console.log(randomNumber);
 }
 
 console.log(randomNum());
