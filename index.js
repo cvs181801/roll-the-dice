b28ef89b6934733e5e4e88f9ff328fa2f88d34b1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-30 10:20:56 -0700

    add a function to roll the dice in js

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 //grab elements from the DOM
-
+const cube = document.querySelector(".cube__container");
+const rollButton = document.querySelector(".roll__dice__btn");
 //create a way to generate a random number
 
 function randomNum() {
-    // const result = Math.floor(Math.random()* 6);
-    // if (result >= 1) {
-    //     return result;
-    // } else {
-    //     return 6;
-    // }
-
-    let randomNumber = getRandomInt(1,7);
-    console.log(randomNumber);
+    const result = Math.floor(Math.random()* 6);
+    if (result >= 1) {
+        return result;
+    } else {
+        return 6;
+    }
 }
 
 console.log(randomNum());
 
+let currentClass="";
+
+function rollDice() {
+    let randomNumber = randomNum();
+    console.log(randomNumber);
+    let showClass = "show__" + randomNumber;
+    console.log(showClass);
+    if (currentClass) {
+cube.classList.remove(currentClass);
+    }
+    cube.classList.add(showClass);
+    currentClass = showClass;
+}
+
+console.log(rollDice());
+
+rollButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    rollDice();
+})
